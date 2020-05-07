2c8895e8490ec24eb8dad053adf69a536c274d86
Sebastián González <sgonzalez@debianchile.cl>
2020-05-07 00:30:25 -0400

    show version on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { name, version } from '../../package.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ShareYourGarden';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.info(`🌱 ${name} version ${version}`);
+  }
 }
